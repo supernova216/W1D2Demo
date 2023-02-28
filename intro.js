47d250a32676634fc1d1ee5c55bcd44ccb24af88
Nathan Harris <nathanharris91@gmail.com>
2023-02-28 15:56:48 -0700

    Practice coding

---
+++ intro.js
@@ -0,0 +1,29 @@
+// console.log("Hello, world")
+
+const firstName = "Nathan"
+// console.log(firstName)
+
+let age = 31
+
+const isCool = true
+
+// console.log(firstName, age, isCool)
+
+// console.log("Hello, my name is " + firstName + " I am " + age + " years old")
+// console.log(`Hello, my name is ${firstName} I am ${age} years old`)
+
+let x = 4
+let y = 10
+let z = x + 8
+let sum = x + y + z
+
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(sum)
+
+if (x === y) {
+console.log(`X and Y are the same`)    
+} else {
+    console.log(`X and Y are different values`)
+}
